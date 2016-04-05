a496a3c242bba133c49c83105dcdff67f7ffe791
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2016-04-05 01:34:53 -0300

    Initial project skeleton - frontend only

---
+++ public/frontend/src/main.js
@@ -0,0 +1,14 @@
+var Promise = require('bluebird'); // Promise polyfill for IE11
+import {bootstrap} from 'aurelia-bootstrapper-webpack';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.css';
+import '../styles/styles.css';
+
+bootstrap(function(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging();
+
+  aurelia.start().then(() => aurelia.setRoot('app', document.body));
+});
